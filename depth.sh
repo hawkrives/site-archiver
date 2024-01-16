ef85738e88b3feb100a2ca988675429c7251724b
Hawken Rives <hawken.rives@techcyte.com>
2024-01-16 16:32:31 -0500

    fix shellcheck issues in depth.sh

--- depth.sh
+++ depth.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
+
 for file in *.sqlite3; do
-  sqlite3 --readonly $file "
+  sqlite3 --readonly "$file" "
     select 
-      '$(basename $file .sqlite3)' as file,
+      '$(basename "$file" .sqlite3)' as file,
        (select count(*) from response) as done,
        (select count(*) from fetch_queue) as fetch,
        (select count(*) from parse_queue) as parse
